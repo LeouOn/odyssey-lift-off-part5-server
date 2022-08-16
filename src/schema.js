e8318434fa4aad7acb907413c5964f9bcdd1003f
Yune Leou-On <yune.leou@gmail.com>
2022-08-16 14:31:27 +0300

    adding one more deprecation changelog

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated (reason:"learning deprecation and shifting to duration in seconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -46,7 +46,7 @@ const typeDefs = gql`
     "The track's complete array of Modules"
     modules: [Module!]!
     "The track's full duration, in seconds"
-durationInSeconds: Int
+    durationInSeconds: Int
   }
 
   "Author of a complete Track or a Module"
@@ -70,7 +70,7 @@ durationInSeconds: Int
     "The module's video url, for video-based modules"
     videoUrl: String
     "The track's full duration, in seconds"
-durationInSeconds: Int
+    durationInSeconds: Int
   }
 `;
 
